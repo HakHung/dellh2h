1276e32f002094ae7c92277adf7d49fd2f421f2c
HakHung <ykehang@gmail.com>
2020-11-10 01:12:48 +0800

    add.php

--- add.php
+++ add.php
@@ -44,31 +44,29 @@ if ($_POST["compulsory"] == "Yes") {
 
     $sql = "INSERT INTO event_table (eventcategory, eventtype, eventname, datepicker, appt, appt1, venue, description, compulsory) values ('$eventcategory','$eventtype', '$eventname','$datepicker', '$appt', '$appt1', '$venue', '$description', '$compulsory')";
     if ($conn->query($sql)) {
-        echo "New record is inserted sucessfully";
+        // echo "New record is inserted sucessfully";
+        // $sql = "SELECT TIMEDIFF(appt1,appt) AS inter FROM event_table";
+
+        // $result = $conn->query($sql);
+        // while($row = $result ->fetch_array()){
+        //     echo $row['inter'] ;
+        //     echo "    ";
+        // }
         if($compulsory=="Yes"){
             $sql = "SELECT MAX(eventid) AS max_eventid FROM event_table";  //  get the eventID of newly inserted
             $eventIdLatest = $conn->query($sql);
             $row = $eventIdLatest ->fetch_array();
-            echo $row["max_eventid"];
             $id = $row["max_eventid"];
-            // while($row = $eventIdLatest->fetch_assoc()){
-            //     echo  $row['eventid'];
-            // }
-            // echo  $eventIdLatest;
             
             $sql = "SELECT userid FROM usertable";
             $alluser =  $conn->query($sql);
 
             while($row = $alluser->fetch_assoc()){
-                // echo  $eventIdLatest;
-                //$sql = "SELECT event_table.eventid FROM event_table, user_event WHERE eventid="+$eventId;
-               // $result1 = $conn->query($sql);
                 $sql = "INSERT INTO user_event(userid, eventid) values (".$row['userid'].",".$id.")";
                 $conn->query($sql);
             }
         }
         include('training.php');
-        // $sql = "SELECT MAX eventid FROM event_table"
     } else {
         "Error: " . $sql . "<br>" . $conn->error;
     }
